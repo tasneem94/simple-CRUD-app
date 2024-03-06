21300bce4bb18ae35c1dc31c5d304e355febf132
tasneem94 <tasneemsakif31@gmail.com>
2024-03-06 22:06:56 +0600

    reformatted

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Product = require("./models/product.model");
+const productRoute = require("./routes/product.route");
 const app = express();
 
 //middleware
@@ -8,71 +9,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
-// app.use("/api/products", productRoute);
+app.use("/api/products", productRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API server");
-});
-
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/api/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//update a product
-app.put("/api/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-
-    if (!product) {
-      return res.status(404).json({ message: "Product not found." });
-    }
-
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//update a product
-app.delete("/api/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id, req.body);
-
-    if (!product) {
-      return res.status(404).json({ message: "Product not found." });
-    }
-
-    res.status(200).json({ message: "Product deleted successfully." });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 });
 
 mongoose
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,2 +1,20 @@
 const express = require("express");
+const Product = require("../models/product.model");
 const router = express.Router();
+const {
+  getProducts,
+  getSingleProducts,
+  createProducts,
+  updateProducts,
+  deleteProducts,
+} = require("../controllers/product.controller");
+
+router.get("/", getProducts);
+
+router.get("/:id", getSingleProducts);
+
+router.post("/", createProducts);
+router.put("/:id", updateProducts);
+router.delete("/", deleteProducts);
+
+module.exports = router;
